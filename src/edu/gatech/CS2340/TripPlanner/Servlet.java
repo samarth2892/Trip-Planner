e37ed284f33a98ee2151e122c7e19eac769fe25b
Samarth <spatel394@gatech.edu>
2014-06-05 00:38:36 -0400

    Added what I have so far

---
+++ src/edu/gatech/CS2340/TripPlanner/Servlet.java
@@ -0,0 +1,20 @@
+package edu.gatech.CS2340.TripPlanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Samarth on 6/4/2014.
+ */
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
