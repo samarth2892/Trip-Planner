757a10a8ddfc8f3131f96ad52f1ff655ac070996
Charles Kilpatrick <ckilpatrick6@gatech.edu>
2014-06-11 13:20:33 -0400

    MySQL Methods Added
    Create account, login, update, encrypted password.

--- src/main/java/edu/gatech/CS2340/TripPlanner/model/Account.java
+++ src/main/java/edu/gatech/CS2340/TripPlanner/model/Account.java
@@ -1,67 +1,8 @@
 package main.java.edu.gatech.CS2340.TripPlanner.model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-/**
- * Created by Charlie on 6/5/2014.
- */
 public class Account {
-
-    static final String DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/accounts";
-
-    static final String USER = "root";
-    static final String PASS = "admin";
-
     public static void main(String[] args) {
-        Connection conn = null;
-        Statement stmt = null;
-        try{
-
-            Class.forName("com.mysql.jdbc.Driver");
-
-
-            System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            System.out.println("Connection sucessful.");
-            
-            stmt = conn.createStatement();
-            
-            String sql = "USE ACCOUNTS";
-            stmt.executeUpdate(sql);
-        }catch(ClassNotFoundException ex) {
-                System.out.println("Error: unable to load driver class!");
-                System.exit(1);
-        }catch(SQLException se){
-
-            se.printStackTrace();
-        }catch(Exception e){
-
-            e.printStackTrace();
-        }finally{
-
-            try{
-                if(stmt!=null)
-                    stmt.close();
-            }catch(SQLException se2){
-            }
-        }
+        AccountDb adb = new AccountDb();
+        adb.connect();
     }
-
-    private String email;
-    private String password;
-
-    public Account(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    public void setEmail(String email) { this.email = email; }
-    public String getEmail() { return email; }
-
-    public void setPassword() { this.password = password; }
-    public String getPassword() { return password; }
 }
