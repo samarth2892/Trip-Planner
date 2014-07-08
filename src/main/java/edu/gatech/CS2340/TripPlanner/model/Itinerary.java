35c0ad58ae4480ac2181ee116fc50e466fe5f465
Charles Kilpatrick <ckilpatrick6@gatech.edu>
2014-07-08 15:58:15 -0400

    created Itinerary class

---
+++ src/main/java/edu/gatech/CS2340/TripPlanner/model/Itinerary.java
@@ -0,0 +1,30 @@
+package main.java.edu.gatech.CS2340.TripPlanner.model;
+import java.util.HashMap;
+
+public class Itinerary {
+    private String origin;
+    private String date;
+    private HashMap map;
+
+    public String getOrigin() {
+        return origin;
+    }
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public HashMap getMap() {
+        return map;
+    }
+    public void setMap(HashMap map) {
+        this.map = map;
+    }
+
+}
