690b5c648a88f028bfd54bacc4d3c828639eaadf
Gina Yu <ginayu@gatech.edu>
2014-07-28 20:10:18 -0400

    testing git hook

--- src/main/java/edu/gatech/CS2340/TripPlanner/controller/forgotPasswordFilter.java
+++ src/main/java/edu/gatech/CS2340/TripPlanner/controller/forgotPasswordFilter.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebFilter(
-        filterName = "forgotPasswordFilter",
+        filterName = "ForgotPasswordFilter",
         urlPatterns = {
                 "/forgotPassword"
         }
